c3e74bb993b36a4f30acd6bf2065aee33d2a4ed1
Amiir <amiir9955@gmail.com>
2026-01-05 12:22:45 +0800

    update testing on apk

--- frontend/bus-planner/app/index.tsx
+++ frontend/bus-planner/app/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Button } from "react-native";
 import { router } from "expo-router";
 import ScreenWrapper from "./screens/screenwrapper";
+import { BACKEND_URL } from "./config/url"; 
 
 // npx expo start --tunnel (tunnel to make it work on mobile)
 // eas update --channel production
@@ -13,7 +14,7 @@ export default function Home() {
           Where are you going?
         </Text>
         <Text style={{ fontSize: 20, marginBottom: 20, textAlign: "center" }}>
-          OTA Test
+          Backend URL: {BACKEND_URL}
         </Text>
 
         <Button
